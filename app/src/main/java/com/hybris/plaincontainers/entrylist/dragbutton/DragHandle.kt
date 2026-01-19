7a6b379f4f31fcab329d81acb74de73c4bce4fb8
Justin Wallis <wallis.justin123@gmail.com>
2026-01-19 17:33:29 +0100

    fix: drag now working for entire vertical component space, not just the icon only

--- app/src/main/java/com/hybris/plaincontainers/entrylist/dragbutton/DragHandle.kt
+++ app/src/main/java/com/hybris/plaincontainers/entrylist/dragbutton/DragHandle.kt
@@ -3,18 +3,20 @@ package com.hybris.plaincontainers.entrylist.dragbutton
 import android.annotation.SuppressLint
 import android.util.Log
 import android.view.MotionEvent
-import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.hybris.plaincontainers.R
 
 class DragHandle(viewHolder: RecyclerView.ViewHolder, dragListener: DragListener) {
 
-    private val btnDrag = viewHolder.itemView.findViewById<ImageButton>(R.id.btnDrag) //view.findViewById<ImageButton>(R.id.btnDrag)
+    private val layoutDrag = viewHolder.itemView.findViewById<ConstraintLayout>(R.id.layoutDrag)
 
     init {
         // Suppress warning for visually impaired for now.
         @SuppressLint("ClickableViewAccessibility")
-        btnDrag.setOnTouchListener { _, e ->
+
+        layoutDrag.setOnTouchListener { _, e ->
             if (e.action == MotionEvent.ACTION_UP || e.action == MotionEvent.ACTION_DOWN) {
                 Log.d("INFO", "OnTouch Dragged " + e.action)
                 dragListener.onStartDrag(viewHolder)
