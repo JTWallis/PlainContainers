969d12a7583b19dfdddaa42fd6d9746a0e2d870c
Justin Wallis <wallis.justin123@gmail.com>
2026-01-19 17:20:09 +0100

    feat: created ViewHolder for EntryDragExpand with some preparation for Expand functionality

---
+++ app/src/main/java/com/hybris/plaincontainers/entrylist/entrydragexpand/EntryDragExpandViewHolder.kt
@@ -0,0 +1,30 @@
+package com.hybris.plaincontainers.entrylist.entrydragexpand
+
+import android.util.Log
+import android.view.View
+import com.hybris.plaincontainers.entrylist.dragbutton.DragHandle
+import com.hybris.plaincontainers.entrylist.dragbutton.DragListener
+import com.hybris.plaincontainers.entrylist.entrybase.EntryBaseViewHolder
+import com.hybris.plaincontainers.entrylist.model.EntryBase
+import com.hybris.plaincontainers.entrylist.model.EntryExpand
+
+class EntryDragExpandViewHolder(view: View, private val dragListener: DragListener)
+    : EntryBaseViewHolder(view) {
+
+    private val dragHandle = DragHandle(this, dragListener)
+
+    init {
+        Log.d("INFO", "Init EntryDragExpandVH")
+    }
+
+    override fun bind(item: EntryBase) {
+        super.bind(item)
+
+        if(item !is EntryExpand) {
+            Log.d("WARNING", "Binding item in EntryDragExpandVH is not of type EntryExpand!")
+            return
+        }
+
+        //(item as EntryExpand).listItems =
+    }
+}
