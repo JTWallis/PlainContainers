3c52b2949b8b06ca6eee9990e2a52d82b55dbf35
Justin Wallis <wallis.justin123@gmail.com>
2026-01-21 16:31:35 +0100

    refactor: added mapping between EntryState and Model for JSON operations

--- app/src/main/java/com/hybris/plaincontainers/data/JsonManager.kt
+++ app/src/main/java/com/hybris/plaincontainers/data/JsonManager.kt
@@ -1,17 +1,11 @@
 package com.hybris.plaincontainers.data
 
 import android.content.Context
-import android.content.res.AssetManager
-import android.util.JsonReader
 import android.util.Log
-import com.hybris.plaincontainers.entrylist.model.EntryContainer
-import kotlinx.serialization.ExperimentalSerializationApi
+import com.hybris.plaincontainers.data.model.EntryContainer
+import com.hybris.plaincontainers.data.states.EntryStateContainer
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromStream
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.InputStream
 
 class JsonManager(context: Context) {
 
@@ -19,7 +13,7 @@ class JsonManager(context: Context) {
     private val PATH = context.filesDir.path + "/$FILENAME"
     private val json = Json{prettyPrint = true}
 
-    fun readContainers(): MutableList<EntryContainer> {
+    fun readContainers(): MutableList<EntryStateContainer> {
         var containers: MutableList<EntryContainer> = ArrayList()
 
         try {
@@ -29,13 +23,14 @@ class JsonManager(context: Context) {
             Log.d("ERROR", "Exception when reading from file $FILENAME: $e")
         }
 
-        return containers
+        return containers.map { e -> EntryStateContainer(e) }.toMutableList()
     }
 
-    fun writeContainers(containers: MutableList<EntryContainer>) {
+    fun writeContainers(containers: MutableList<EntryStateContainer>) {
+        val models = containers.map{ e -> e.model}
         try {
             val f = File(PATH)
-            f.writeText(json.encodeToString(containers))
+            f.writeText(json.encodeToString(models))
         } catch(e: Exception) {
             Log.d("ERROR", "Exception when writing to file $FILENAME: $e")
         }
