5a7bb96194d6bb12ef3823ee1d95ce62d4f2b89a
Justin Wallis <wallis.justin123@gmail.com>
2026-01-23 13:25:01 +0100

    feat: implemented Popup-subclass for sorting popup

---
+++ app/src/main/java/com/hybris/plaincontainers/views/sortpopup/SortPopup.kt
@@ -0,0 +1,57 @@
+package com.hybris.plaincontainers.views.sortpopup
+
+import android.util.Log
+import android.view.View
+import android.widget.ListView
+import android.widget.TextView
+import com.hybris.plaincontainers.R
+import com.hybris.plaincontainers.views.Popup
+
+class SortPopup(
+    view: View,
+    private var sortSelection: SortSelection,
+    private val onSortChanged: (SortSelection) -> Unit)
+: Popup(view, R.layout.popup_list) {
+
+    private val txtTitle = contentView.findViewById<TextView>(R.id.txtPopupListTitle)
+    private val list = contentView.findViewById<ListView>(R.id.lvPopupList)
+    // TODO: Add map from enum value to (localized) string value
+    private val sortOptions: Array<String> = arrayOf(
+        "Name",
+        "Date added",
+        "Date modified",
+        "Custom"
+    )
+
+    private val adapter = SortPopupAdapter(
+        view.context,
+        sortOptions,
+        sortSelection,
+        arrayOf(sortOptions.lastIndex),
+        onClick = {e -> onSortOptionClicked(e)})
+
+    init {
+        list.adapter = adapter
+    }
+
+    fun setTitle(text: String) {
+        txtTitle.text = text
+    }
+
+    private fun onSortOptionClicked(position: Int) {
+        Log.d("INFO", "Clicked position $position")
+
+        val selectedPos = sortSelection.option.ordinal
+
+        if(selectedPos == position) {
+            sortSelection.isAscending = !sortSelection.isAscending
+        } else {
+            sortSelection.isAscending = true
+            sortSelection.option = SortOption.entries[position]
+        }
+
+        onSortChanged(sortSelection)
+        adapter.notifyDataSetChanged()
+    }
+
+}
