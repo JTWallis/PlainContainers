9d81d3bb324cfc341e5dabab7a35b814179c4ab4
Justin Wallis <wallis.justin123@gmail.com>
2026-01-23 13:19:13 +0100

    feat: created generic, extendable popup window class

---
+++ app/src/main/java/com/hybris/plaincontainers/views/Popup.kt
@@ -0,0 +1,26 @@
+package com.hybris.plaincontainers.views
+
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.annotation.LayoutRes
+
+open class Popup(
+    view: View,
+    @LayoutRes layoutRes: Int,
+) {
+
+    protected val contentView: View = LayoutInflater.from(view.context).inflate(layoutRes, null)
+    private val popupWindow = PopupWindow(
+        contentView,
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        true
+    )
+
+    fun show(view: View, gravity: Int = Gravity.CENTER, x: Int = 0, y: Int = 0) {
+        popupWindow.showAtLocation(view, gravity, x, y)
+    }
+}
