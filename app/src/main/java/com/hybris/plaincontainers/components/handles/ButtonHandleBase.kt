e45f2907ead0ceaf936847811162b434c64ffc91
Justin Wallis <wallis.justin123@gmail.com>
2026-01-23 15:27:13 +0100

    feat: created generic, extendable class for simple button handles

---
+++ app/src/main/java/com/hybris/plaincontainers/components/handles/ButtonHandleBase.kt
@@ -0,0 +1,31 @@
+package com.hybris.plaincontainers.components.handles
+
+import android.view.View
+import android.widget.Button
+import androidx.annotation.IdRes
+import androidx.cardview.widget.CardView
+
+open class ButtonHandleBase(
+    view: View,
+    onClick: () -> Unit,
+    @IdRes cardviewId: Int,
+    @IdRes buttonId: Int) {
+
+    protected val cvHandle = view.findViewById<CardView>(cardviewId)
+    protected val btnHandle = view.findViewById<Button>(buttonId)!!
+
+    init {
+        btnHandle.setOnClickListener { onClick() }
+    }
+
+    open fun setText(text: String) {
+        btnHandle.text = text
+    }
+
+    open fun setVisibility(visible: Boolean) {
+        cvHandle.visibility =
+            if(visible) View.VISIBLE
+            else View.INVISIBLE
+    }
+
+}
