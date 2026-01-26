a2afc84ddd52d436185209d672bd01b3952eb4c7
Justin Wallis <wallis.justin123@gmail.com>
2026-01-26 12:55:26 +0100

    feat: created handle for managing item count display

---
+++ app/src/main/java/com/hybris/plaincontainers/components/handles/CountHandle.kt
@@ -0,0 +1,42 @@
+package com.hybris.plaincontainers.components.handles
+
+import android.view.View
+import android.widget.TextView
+import com.hybris.plaincontainers.R
+
+class CountHandle(
+    view: View,
+    private var count: Int,
+    private val onZeroCount: () -> Unit
+) {
+
+    private val tvHandle = view.findViewById<TextView>(R.id.tvCount)
+
+    init {
+        updateCountText()
+    }
+
+    fun setCount(newCount: Int) {
+        if(newCount <= 0) {
+            count = 0
+            onZeroCount()
+        } else {
+            count = newCount
+        }
+
+        updateCountText()
+    }
+
+    fun incrementCount() {
+        setCount(count + 1)
+    }
+
+    fun decrementCount() {
+        setCount(count - 1)
+    }
+
+    private fun updateCountText() {
+        tvHandle.text = "${count}x"
+    }
+
+}
