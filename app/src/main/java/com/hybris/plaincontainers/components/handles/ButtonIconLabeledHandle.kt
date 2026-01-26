7d19e637fae7710d3ed299c114d6b7b7dda2585f
Justin Wallis <wallis.justin123@gmail.com>
2026-01-26 19:33:44 +0100

    feat: implemented inheritable handle for buttons with text and icon

---
+++ app/src/main/java/com/hybris/plaincontainers/components/handles/ButtonIconLabeledHandle.kt
@@ -0,0 +1,38 @@
+package com.hybris.plaincontainers.components.handles
+
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import com.hybris.plaincontainers.R
+
+open class ButtonIconLabeledHandle(
+    view: View,
+    onClick: () -> Unit,
+    buttonText: String,
+    @DrawableRes iconId: Int,
+    placeIconToEnd: Boolean = false,
+) : ButtonHandleBase(
+    view,
+    onClick,
+    R.id.cvBtnLabeled,
+    R.id.btnLabeled
+) {
+
+    init {
+        val drawable = ContextCompat.getDrawable(view.context, iconId)
+        var start: Drawable?
+        var end: Drawable?
+        if(placeIconToEnd) {
+            start = null
+            end = drawable
+        } else {
+            start = drawable
+            end = null
+        }
+
+        btnHandle.setCompoundDrawablesWithIntrinsicBounds(start, null, end, null)
+        btnHandle.text = buttonText
+    }
+
+}
