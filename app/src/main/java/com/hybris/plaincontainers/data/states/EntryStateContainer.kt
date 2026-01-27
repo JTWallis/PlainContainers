c200daf3aa9ddcabae58fb929456ec5763ef284a
Justin Wallis <wallis.justin123@gmail.com>
2026-01-27 16:27:58 +0100

    feat: removed EntryState wrappers

--- app/src/main/java/com/hybris/plaincontainers/data/states/EntryStateContainer.kt
+++
@@ -1,7 +0,0 @@
-package com.hybris.plaincontainers.data.states
-
-import com.hybris.plaincontainers.data.model.EntryContainer
-
-class EntryStateContainer(model: EntryContainer) : EntryStateBase<EntryContainer>(model) {
-    var isExpanded = false
-}
--- app/src/main/java/com/hybris/plaincontainers/data/states/EntryStateItem.kt
+++
@@ -1,6 +0,0 @@
-package com.hybris.plaincontainers.data.states
-
-import com.hybris.plaincontainers.data.model.EntryItem
-
-class EntryStateItem(model: EntryItem): EntryStateBase<EntryItem>(model) {
-}
