271995939c6618c164948666b5b09c3702dc1a13
Justin Wallis <wallis.justin123@gmail.com>
2026-01-27 16:49:49 +0100

    feat: made serializable

--- app/src/main/java/com/hybris/plaincontainers/views/sortpopup/SortSelection.kt
+++ app/src/main/java/com/hybris/plaincontainers/views/sortpopup/SortSelection.kt
@@ -1,5 +1,8 @@
 package com.hybris.plaincontainers.views.sortpopup
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class SortSelection(
     var option: SortOption,
     var isAscending: Boolean
