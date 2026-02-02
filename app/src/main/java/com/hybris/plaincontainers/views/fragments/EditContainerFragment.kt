3d44ffef980447675b8334cfcdee37cef2c0052f
Justin Wallis <wallis.justin123@gmail.com>
2026-02-02 15:15:56 +0100

    feat: created choice popup for delete confirmation

--- app/src/main/java/com/hybris/plaincontainers/views/fragments/EditContainerFragment.kt
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/EditContainerFragment.kt
@@ -1,9 +1,11 @@
 package com.hybris.plaincontainers.views.fragments
 
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.hybris.plaincontainers.data.JsonManager
 import com.hybris.plaincontainers.data.fragmentargs.EditContainerFragmentArg
 import com.hybris.plaincontainers.data.model.EntryContainer
+import com.hybris.plaincontainers.views.choicepopup.ChoicePopup
 import java.io.Serializable
 
 class EditContainerFragment(): MetadataContainerFragment() {
@@ -49,8 +51,19 @@ class EditContainerFragment(): MetadataContainerFragment() {
         findNavController().navigateUp()
     }
 
-    override fun onBtnDeleteClick() {
+    override fun onBtnDeleteClick(view: View) {
+        val popup = ChoicePopup(
+            view,
+            onClickLeft = { },
+            onClickRight = { onBtnDeleteConfirmClick() },
+            true
+        )
+        popup.setTextTitle("Do you really want to delete this container, including its item entries?")
+        popup.setTextSubtitle("This action is irreversible!")
+        popup.setTextButtonLeft("Cancel")
+        popup.setTextButtonRight("Delete!")
 
+        popup.show(view)
     }
 
     override fun getAppbarTitle(): String {
