914bdffe5d62387e129b97741a82a1974e8f7a7b
Justin Wallis <wallis.justin123@gmail.com>
2026-02-04 14:48:50 +0100

    feat: created helper class for File functions

---
+++ app/src/main/java/com/hybris/plaincontainers/data/FileUtils.kt
@@ -0,0 +1,14 @@
+package com.hybris.plaincontainers.data
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.core.net.toFile
+import androidx.core.net.toUri
+import java.io.File
+
+object FileUtils {
+
+
+}
