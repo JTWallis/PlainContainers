9fb78257a601106849285b994ebaedf9d619e9d7
Justin Wallis <wallis.justin123@gmail.com>
2026-02-04 18:56:49 +0100

    feat: created Capture fragment

---
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/CaptureFragment.kt
@@ -0,0 +1,56 @@
+package com.hybris.plaincontainers.views.fragments
+
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.hybris.plaincontainers.R
+import com.hybris.plaincontainers.data.FileUtils
+import java.io.File
+import java.io.Serializable
+
+
+class CaptureFragment : FragmentBase(R.layout.fragment_capture) {
+
+    private lateinit var viewCrop: View
+    private lateinit var btnCapture: Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun initViews(view: View) {
+        viewCrop = view.findViewById(R.id.viewCaptureCrop)
+        btnCapture = view.findViewById(R.id.btnCapture)
+    }
+
+
+    override fun initPackageData() {}
+
+    override fun getAppbarTitle(): String {
+        return "Capture Photo"
+    }
+
+    override fun getAppbarSubtitle(): String {
+        return ""
+    }
+
+    override fun getContainerPackage(): Serializable {
+        return object: Serializable {}
+    }
+}
