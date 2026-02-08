6aa4b17c9e274a74e99126aaf16cd03239dce180
Justin Wallis <wallis.justin123@gmail.com>
2026-02-08 17:25:58 +0100

    feat: setup Locale helper object to set and persist a supported locale

---
+++ app/src/main/java/com/hybris/plaincontainers/data/LocaleUtils.kt
@@ -0,0 +1,49 @@
+package com.hybris.plaincontainers.data
+
+import android.content.Context
+import android.preference.PreferenceManager
+import java.util.Locale
+
+object LocaleUtils {
+
+    private val SELECTED_LANG = "Locale.Helper.Selected.Language"
+
+    fun setLocale(context: Context, supportedLocale: SupportedLocale): Context {
+        val language = supportedLocale.toLanguageTag()
+        persist(context, language)
+        return updateResources(context, language)
+    }
+
+    fun getLocale(context: Context): Locale {
+        val lang = PreferenceManager
+            .getDefaultSharedPreferences(context)
+            .getString(SELECTED_LANG, Locale.getDefault().language)!!
+        return Locale.forLanguageTag(lang)
+    }
+
+    fun getLocaleFromLanguageTag(languageTag: String): SupportedLocale {
+        for(locale in SupportedLocale.entries) {
+            if(languageTag == locale.toLanguageTag()) return locale
+        }
+
+        return SupportedLocale.EN
+    }
+
+    private fun persist(context: Context, lang: String) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .edit()
+            .putString(SELECTED_LANG, lang)
+            .apply()
+    }
+
+    private fun updateResources(context: Context, lang: String): Context {
+        val locale = Locale(lang)
+        Locale.setDefault(locale)
+
+        val config = context.resources.configuration
+        config.setLocale(locale)
+        config.setLayoutDirection(locale)
+
+        return context.createConfigurationContext(config)
+    }
+}
