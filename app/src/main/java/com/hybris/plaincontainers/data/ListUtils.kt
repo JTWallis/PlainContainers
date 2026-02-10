a2acbbc5d0c2e90c3235ec0a13ed7aca69d179ff
Justin Wallis <wallis.justin123@gmail.com>
2026-02-10 20:06:56 +0100

    feat: added sorting by dateAdded and -modified

--- app/src/main/java/com/hybris/plaincontainers/data/ListUtils.kt
+++ app/src/main/java/com/hybris/plaincontainers/data/ListUtils.kt
@@ -12,8 +12,14 @@ object ListUtils {
                 if(sortParams.isAscending) items.sortBy { e -> e.name.uppercase() }
                 else items.sortByDescending { e -> e.name.uppercase() }
             }
-            SortOption.DATE_ADDED -> {}
-            SortOption.DATE_MODIFIED -> {}
+            SortOption.DATE_ADDED -> {
+                if(sortParams.isAscending) items.sortBy { e -> e.dateAdded }
+                else items.sortByDescending { e -> e.dateAdded }
+            }
+            SortOption.DATE_MODIFIED -> {
+                if(sortParams.isAscending) items.sortBy { e -> e.dateModified }
+                else items.sortByDescending { e -> e.dateModified }
+            }
             SortOption.CUSTOM -> {}
         }
     }
