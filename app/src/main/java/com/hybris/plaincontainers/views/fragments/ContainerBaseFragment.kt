d601ad94d7a4f2351d91fae55b423615997e8acf
Justin Wallis <wallis.justin123@gmail.com>
2026-02-11 17:47:56 +0100

    feat: applied DragEnabled state globally on switch check

--- app/src/main/java/com/hybris/plaincontainers/views/fragments/ContainerBaseFragment.kt
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/ContainerBaseFragment.kt
@@ -14,6 +14,7 @@ import com.hybris.plaincontainers.components.handles.buttoniconlabeled.AddHandle
 import com.hybris.plaincontainers.components.handles.buttoniconlabeled.EditHandle
 import com.hybris.plaincontainers.components.handles.buttoniconlabeled.SortHandle
 import com.hybris.plaincontainers.data.ListUtils
+import com.hybris.plaincontainers.data.SettingsManager
 import com.hybris.plaincontainers.data.model.EntryBase
 import com.hybris.plaincontainers.entrylist.dragbutton.DragAdapter
 import com.hybris.plaincontainers.entrylist.dragbutton.DragListener
@@ -56,6 +57,7 @@ abstract class ContainerBaseFragment<T: EntryBase>(): FragmentBase(R.layout.acti
         initRecycleView(view)
 
         switchDrag.setOnCheckedChangeListener { _, isChecked ->
+            SettingsManager.setDragEnabled(isChecked)
             rcvAdapter.setDragVisibility(isChecked)
         }
 
