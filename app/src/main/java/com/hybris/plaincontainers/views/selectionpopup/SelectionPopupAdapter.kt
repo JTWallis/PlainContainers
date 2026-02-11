d15ecc6b69866c3c8374fa069360c8f9c85c743a
Justin Wallis <wallis.justin123@gmail.com>
2026-02-11 16:54:32 +0100

    feat: created popup for radio selections

---
+++ app/src/main/java/com/hybris/plaincontainers/views/selectionpopup/SelectionPopupAdapter.kt
@@ -0,0 +1,54 @@
+package com.hybris.plaincontainers.views.selectionpopup
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.RadioButton
+import com.hybris.plaincontainers.R
+
+class SelectionPopupAdapter(
+    context: Context,
+    private val data: Array<String>,
+    private var selection: Int,
+    private val onClick: (position: Int) -> Unit
+): BaseAdapter() {
+    private val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return data.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return data[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+
+        var view: RadioButton? = convertView as RadioButton?
+        if(view == null) {
+            view = inflater.inflate(R.layout.component_selection, null) as RadioButton
+        }
+
+        view.text = data[position]
+        view.isChecked = (position == selection)
+        view.setOnClickListener {
+            if(position != selection) {
+                selection = position
+                onClick(position)
+                notifyDataSetChanged()
+            }
+        }
+
+        return view
+    }
+}
