7b251696472240f92ee2b210d761e1ffe2c0f275
Justin Wallis <wallis.justin123@gmail.com>
2026-02-11 14:07:24 +0100

    refactor: now setting the appbar Menu through the MenuProvider API

--- app/src/main/java/com/hybris/plaincontainers/MainActivity.kt
+++ app/src/main/java/com/hybris/plaincontainers/MainActivity.kt
@@ -3,7 +3,6 @@ package com.hybris.plaincontainers
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -13,7 +12,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.hybris.plaincontainers.data.JsonManager
 import com.hybris.plaincontainers.data.LocaleUtils
-import com.hybris.plaincontainers.data.SupportedLocale
 import com.hybris.plaincontainers.data.model.EntryContainer
 import com.hybris.plaincontainers.data.model.EntryItem
 import com.hybris.plaincontainers.data.model.RootContainer
@@ -67,8 +65,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        layoutToolbar.setOnMenuItemClickListener { menuItem -> onOptionsItemSelected(menuItem) }
-
         layoutToolbar.setNavigationOnClickListener {
             navController.navigateUp()
         }
@@ -87,30 +83,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            android.R.id.home -> {
-                onBackPressedDispatcher.onBackPressed()
-                true
-            }
-            R.id.actionSettings -> {
-                //findNavController().navigate(R.id.)
-                // DEBUG Change to German
-                LocaleUtils.setLocale(this, SupportedLocale.DE)
-                recreate()
-                true
-            }
-            R.id.actionAbout -> {
-                // DEBUG Change to English
-                LocaleUtils.setLocale(this, SupportedLocale.EN)
-                recreate()
-                true
-            }
-            else -> {
-                super.onOptionsItemSelected(item)
-            }
-        }
-    }
 
     override fun attachBaseContext(base: Context) {
         val langTag = LocaleUtils.getLocale(base).toLanguageTag()
--- app/src/main/java/com/hybris/plaincontainers/views/fragments/FragmentBase.kt
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/FragmentBase.kt
@@ -3,10 +3,20 @@ package com.hybris.plaincontainers.views.fragments
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import com.hybris.plaincontainers.R
+import com.hybris.plaincontainers.data.LocaleUtils
+import com.hybris.plaincontainers.data.SupportedLocale
 import com.hybris.plaincontainers.data.appbar.AppBarModel
 import com.hybris.plaincontainers.data.appbar.AppBarViewModel
 import java.io.Serializable
@@ -19,6 +29,7 @@ abstract class FragmentBase(@LayoutRes contentLayoutId: Int) : Fragment(contentL
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initMenuProvider()
         initPackageData()
         initViews(view)
         initAppbarSubtitle()
@@ -33,6 +44,43 @@ abstract class FragmentBase(@LayoutRes contentLayoutId: Int) : Fragment(contentL
     protected abstract fun getContainerPackage(): Serializable
 
     protected open fun initAppbarSubtitle() {}
+
+    private fun initMenuProvider() {
+        val menuHost: MenuHost = requireActivity()
+        val menuId = R.menu.toolbar_menu
+
+        menuHost.addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(menuId, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return onAppbarMenuItemSelected(menuItem)
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun onAppbarMenuItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.actionSettings -> {
+                findNavController().navigate(R.id.action_any_to_settings)
+                // DEBUG Change to German
+                //LocaleUtils.setLocale(this, SupportedLocale.DE)
+                //recreate()
+                true
+            }
+            R.id.actionAbout -> {
+                // DEBUG Change to English
+                LocaleUtils.setLocale(requireContext(), SupportedLocale.DE)
+                requireActivity().recreate()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
 
     protected fun <T: Serializable?> getSerializable(name: String, clazz: Class<T>): T {
         Log.d("INFO", "Getting $name with class $clazz")
