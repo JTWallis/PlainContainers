56e150a1b42da91befa55fe011faa4c7328fcd9e
Justin Wallis <wallis.justin123@gmail.com>
2026-02-11 16:55:52 +0100

    feat: setup button click

--- app/src/main/java/com/hybris/plaincontainers/views/fragments/SettingsFragment.kt
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/SettingsFragment.kt
@@ -2,18 +2,30 @@ package com.hybris.plaincontainers.views.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.hybris.plaincontainers.R
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+    private lateinit var btnChangeLanguage: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
+        initViews(view)
+        initListeners()
     }
 
-    private fun initViews() {
+    private fun initViews(view: View) {
+        btnChangeLanguage = view.findViewById(R.id.btnSettingsLanguage)
+    }
+
+    private fun initListeners() {
+        btnChangeLanguage.setOnClickListener { onBtnChangeLanguageClick() }
+    }
+
+    private fun onBtnChangeLanguageClick() {
 
     }
+
 
 }
