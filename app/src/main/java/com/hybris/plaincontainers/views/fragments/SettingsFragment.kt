134fb4aa8832176d8c9fb75731760700401bfdc0
Justin Wallis <wallis.justin123@gmail.com>
2026-02-11 14:05:10 +0100

    feat: setup basic fragment for settings

---
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/SettingsFragment.kt
@@ -0,0 +1,19 @@
+package com.hybris.plaincontainers.views.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.hybris.plaincontainers.R
+
+class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+
+    }
+
+}
