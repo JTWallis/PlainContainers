b597eff699c6688bc7195db0104475703821a8b3
Justin Wallis <wallis.justin123@gmail.com>
2026-02-13 00:23:16 +0100

    feat: added optional backgroundColor

--- app/src/main/java/com/hybris/plaincontainers/components/handles/itemcount/CountHandle.kt
+++ app/src/main/java/com/hybris/plaincontainers/components/handles/itemcount/CountHandle.kt
@@ -2,17 +2,20 @@ package com.hybris.plaincontainers.components.handles.itemcount
 
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.ColorRes
 import com.hybris.plaincontainers.R
 
 class CountHandle(
     private val view: View,
     private var count: Int,
-    private val onZeroCount: () -> Unit
+    private val onZeroCount: () -> Unit,
+    @ColorRes backgroundColorId: Int = R.color.backgroundHighlight
 ) {
 
     private val tvHandle = view.findViewById<TextView>(R.id.tvCount)
 
     init {
+        view.setBackgroundColor(view.context.getColor(backgroundColorId))
         updateCountText()
     }
 
