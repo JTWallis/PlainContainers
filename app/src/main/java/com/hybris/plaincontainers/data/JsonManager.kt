6627cf5784c5922e72f6c517bb2b148a3da72186
Justin Wallis <wallis.justin123@gmail.com>
2026-02-17 17:25:25 +0100

    feat: removed old model functions and data

--- app/src/main/java/com/hybris/plaincontainers/data/JsonManager.kt
+++ app/src/main/java/com/hybris/plaincontainers/data/JsonManager.kt
@@ -2,9 +2,6 @@ package com.hybris.plaincontainers.data
 
 import android.content.Context
 import android.util.Log
-import com.hybris.plaincontainers.data.model.EntryContainer
-import com.hybris.plaincontainers.data.model.EntryItem
-import com.hybris.plaincontainers.data.model.RootContainer
 import com.hybris.plaincontainers.data.model.Settings
 import com.hybris.plaincontainers.views.sortpopup.SortOption
 import com.hybris.plaincontainers.views.sortpopup.SortSelection
@@ -14,29 +11,13 @@ import java.io.File
 object JsonManager {
 
     private var isInit = false
-    private val FILENAME = "containers.json"
     private const val FILENAME_SETTINGS = "settings.json"
-    private var PATH = ""
     private var PATH_SETTINGS = ""
-    private val json = Json{prettyPrint = true}
     private val jsonSettings = Json{ prettyPrint = true }
-    private lateinit var root: RootContainer
 
     fun init(context: Context) {
         val rootPath = FileUtils.getRootPath(context)
-        PATH = FileUtils.getRootPath(context) + "/$FILENAME"
         PATH_SETTINGS = "${rootPath}/${FILENAME_SETTINGS}"
-
-        val rootNullable = readRoot()
-        if(rootNullable == null) {
-            root = RootContainer(
-                SortSelection(SortOption.CUSTOM, true),
-                emptyList()
-            )
-            writeRoot(root)
-        } else {
-            root = rootNullable
-        }
 
         val settingsNullable = readSettings()
         SettingsManager.init(settingsNullable)
@@ -53,7 +34,7 @@ object JsonManager {
             val file = File(PATH_SETTINGS)
             return Json.decodeFromString(file.readText())
         } catch(e: Exception) {
-            Log.d("ERROR", "Exception when reading from file $FILENAME: $e")
+            Log.d("ERROR", "Exception when reading from file $FILENAME_SETTINGS: $e")
         }
 
         return null
@@ -64,143 +45,8 @@ object JsonManager {
             val f = File(PATH_SETTINGS)
             f.writeText(jsonSettings.encodeToString(settings))
         } catch(e: Exception) {
-            Log.d("ERROR", "Exception when writing to file $FILENAME: $e")
+            Log.d("ERROR", "Exception when writing to file $FILENAME_SETTINGS: $e")
         }
-    }
-
-    fun readRoot(): RootContainer? {
-        try {
-            val file = File(PATH)
-            return Json.decodeFromString(file.readText())
-        } catch(e: Exception) {
-            Log.d("ERROR", "Exception when reading from file $FILENAME: $e")
-        }
-
-        return null
-    }
-
-    fun writeRoot(rootContainer: RootContainer) {
-        try {
-            val f = File(PATH)
-            f.writeText(json.encodeToString(rootContainer))
-        } catch(e: Exception) {
-            Log.d("ERROR", "Exception when writing to file $FILENAME: $e")
-        }
-    }
-
-    fun getRoot(): RootContainer {
-        verifyRoot()
-        return root
-    }
-
-    fun readContainers(): MutableList<EntryContainer> {
-        val root = readRoot() ?: return ArrayList()
-        return root.containers.toMutableList()
-    }
-
-    fun writeContainers(containers: List<EntryContainer>) {
-        verifyRoot()
-
-        root = RootContainer(root.sortParams, containers)
-        writeRoot(root)
-    }
-
-    fun getContainers(): List<EntryContainer> {
-        verifyRoot()
-        return root.containers
-    }
-
-    fun writeContainer(containerPos: Int, container: EntryContainer) {
-        verifyRoot()
-
-        val newContainers = root.containers.toMutableList()
-        newContainers[containerPos] = container
-        ListUtils.sortEntryList(newContainers, root.sortParams)
-
-        writeContainers(newContainers)
-    }
-
-    fun addContainer(container: EntryContainer) {
-        verifyRoot()
-
-        val newContainers = root.containers.toMutableList()
-        newContainers.add(container)
-        ListUtils.sortEntryList(newContainers, root.sortParams)
-
-        writeContainers(newContainers)
-    }
-
-    fun removeContainer(containerPos: Int) {
-        verifyRoot()
-
-        val newContainers = root.containers.toMutableList()
-        newContainers.removeAt(containerPos)
-
-        writeContainers(newContainers)
-    }
-
-    fun getContainer(containerPos: Int): EntryContainer {
-        verifyRoot()
-        return root.containers[containerPos]
-    }
-
-    fun writeItems(containerPos: Int, items: List<EntryItem>) {
-        if(containerPos < 0) return
-
-        val container = root.containers[containerPos]
-
-        val newContainer = EntryContainer(
-            container.name,
-            container.thumbnailSrc,
-            container.description,
-            container.dateAdded,
-            container.dateModified,
-            container.color,
-            container.sortParams,
-            items
-        )
-
-        writeContainer(containerPos, newContainer)
-    }
-
-    fun getItems(containerPos: Int): List<EntryItem> {
-        verifyRoot()
-        return root.containers[containerPos].items
-    }
-
-    fun writeItem(containerPos: Int, itemPos: Int, entryItem: EntryItem) {
-        verifyRoot()
-
-        val container = root.containers[containerPos]
-        val newItems = container.items.toMutableList()
-        newItems[itemPos] = entryItem
-        ListUtils.sortEntryList(newItems, container.sortParams)
-
-        writeItems(containerPos, newItems)
-    }
-
-    fun addItem(containerPos: Int, entryItem: EntryItem) {
-        verifyRoot()
-
-        val container = root.containers[containerPos]
-        val newItems = container.items.toMutableList()
-        newItems.add(entryItem)
-        ListUtils.sortEntryList(newItems, container.sortParams)
-
-        writeItems(containerPos, newItems)
-    }
-
-    fun removeItem(containerPos: Int, itemPos: Int) {
-        verifyRoot()
-
-        val newItems = root.containers[containerPos].items.toMutableList()
-        newItems.removeAt(itemPos)
-        writeItems(containerPos, newItems)
-    }
-
-    fun getItem(containerPos: Int, itemPos: Int): EntryItem {
-        verifyRoot()
-        return root.containers[containerPos].items[itemPos]
     }
 
     private fun throwExceptionInit() {
@@ -213,7 +59,6 @@ object JsonManager {
 
     private fun verifyRoot() {
         if(!isInit) throwExceptionInit()
-        if(!this::root.isInitialized) throwExceptionNull()
     }
 
 }
