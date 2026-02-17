0f34c5c61688776ef983c34eee2a410193fb3b4e
Justin Wallis <wallis.justin123@gmail.com>
2026-02-17 16:49:36 +0100

    feat: created repository classes for structured access to the Daos

---
+++ app/src/main/java/com/hybris/plaincontainers/data/repositories/EntryBaseRepository.kt
@@ -0,0 +1,20 @@
+package com.hybris.plaincontainers.data.repositories
+
+import com.hybris.plaincontainers.data.daos.EntryBaseDao
+import com.hybris.plaincontainers.data.entities.EntryBase
+
+abstract class EntryBaseRepository<T: EntryBase, V: EntryBaseDao<T>> {
+    protected abstract val dao: V
+
+    suspend fun update(vararg obj: T) {
+        dao.update(*obj)
+    }
+
+    suspend fun insert(obj: T) {
+        dao.insert(obj)
+    }
+
+    suspend fun delete(obj: T) {
+        dao.delete(obj)
+    }
+}
---
+++ app/src/main/java/com/hybris/plaincontainers/data/repositories/EntryContainerRepository.kt
@@ -0,0 +1,53 @@
+package com.hybris.plaincontainers.data.repositories
+
+import com.hybris.plaincontainers.data.AppDatabaseManager
+import com.hybris.plaincontainers.data.daos.EntryContainerDao
+import com.hybris.plaincontainers.data.entities.EntryContainer
+import com.hybris.plaincontainers.views.sortpopup.SortOption
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+
+class EntryContainerRepository: EntryBaseRepository<EntryContainer, EntryContainerDao>() {
+    override val dao = AppDatabaseManager.get().containerDao()
+    val allContainersOrdered: Flow<List<EntryContainer>> = getAllOrdered()
+
+    fun get(containerId: Long): Flow<EntryContainer?> {
+        return dao.getById(containerId)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun getAllOrdered(): Flow<List<EntryContainer>> {
+        return AppDatabaseManager.get().rootDao().get().flatMapLatest { root ->
+
+            return@flatMapLatest when(root.sortOption) {
+                SortOption.NAME.ordinal -> {
+                    if(root.sortAscending) dao.getAllOrderByNameAsc()
+                    else dao.getAllOrderByNameDesc()
+                }
+                SortOption.DATE_ADDED.ordinal -> {
+                    if(root.sortAscending) dao.getAllOrderByDateAddedAsc()
+                    else dao.getAllOrderByDateAddedDesc()
+                }
+                SortOption.DATE_MODIFIED.ordinal -> {
+                    if(root.sortAscending) dao.getAllOrderByDateModifiedAsc()
+                    else dao.getAllOrderByDateModifiedDesc()
+                }
+                SortOption.CUSTOM.ordinal -> {
+                    dao.getAllOrderBySortPos()
+                }
+                else -> {
+                    dao.getAll()
+                }
+            }
+        }
+    }
+
+    suspend fun updateSortParams(containerId: Long, sortOptionOrdinal: Int, isAscending: Boolean) {
+        dao.updateSortParams(containerId, sortOptionOrdinal, isAscending)
+    }
+
+    suspend fun count(): Int {
+        return dao.count()
+    }
+}
