952f26f746bddd37d21d10e3053c177ba1747268
Justin Wallis <wallis.justin123@gmail.com>
2026-02-17 16:31:51 +0100

    feat: added singleton for accessing and building AppDatabase

---
+++ app/src/main/java/com/hybris/plaincontainers/data/AppDatabaseManager.kt
@@ -0,0 +1,36 @@
+package com.hybris.plaincontainers.data
+
+import android.content.Context
+import android.util.Log
+import androidx.room.Room
+
+object AppDatabaseManager {
+    private lateinit var appDatabase: AppDatabase
+    private var isInit = false
+
+    fun init(context: Context) {
+        try {
+            appDatabase = Room.databaseBuilder(
+                context,
+                AppDatabase::class.java, "plaincontainers-database"
+            )
+            .build()
+        } catch(e: Exception) {
+            Log.e("ERROR", "Exception when building AppDatabase: $e")
+        }
+
+
+        isInit = true
+    }
+
+    fun get(): AppDatabase {
+        verifyInit()
+        return appDatabase
+    }
+
+    private fun verifyInit() {
+        if(!isInit || !::appDatabase.isInitialized) {
+            throw NullPointerException("AppDatabaseBuilder has not been initialized!")
+        }
+    }
+}
