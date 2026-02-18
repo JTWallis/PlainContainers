75509eb17358984f8f25e6732214a3862dd43bfb
Justin Wallis <wallis.justin123@gmail.com>
2026-02-18 15:20:51 +0100

    refactor: inheriting ViewModel instead of MetadataBaseViewModel and removed artificial entry Flow

--- app/src/main/java/com/hybris/plaincontainers/data/viewmodels/AddItemViewModel.kt
+++ app/src/main/java/com/hybris/plaincontainers/data/viewmodels/AddItemViewModel.kt
@@ -1,19 +1,13 @@
 package com.hybris.plaincontainers.data.viewmodels
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hybris.plaincontainers.data.entities.EntryItem
 import com.hybris.plaincontainers.data.repositories.EntryItemRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
-class AddItemViewModel(containerId: Long) : MetadataBaseViewModel() {
+class AddItemViewModel(containerId: Long) : ViewModel() {
     private val itemRepository = EntryItemRepository(containerId)
-
-    // Immediately emit this inherited flow, as there is no ViewData to fill.
-    override val entry: Flow<Int> = flow {
-        emit(1)
-    }
 
     fun insertToContainer(item: EntryItem) {
         viewModelScope.launch {
