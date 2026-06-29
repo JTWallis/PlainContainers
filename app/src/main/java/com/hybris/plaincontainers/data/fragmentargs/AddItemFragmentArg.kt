e801c352f2ed3aa5c08253e23ab6fcf227478a8d
Justin Wallis <wallis.justin123@gmail.com>
2026-06-29 16:10:58 +0200

    feat: added consumable flag for navigation to BarcodeFragment

--- app/src/main/java/com/hybris/plaincontainers/data/fragmentargs/AddItemFragmentArg.kt
+++ app/src/main/java/com/hybris/plaincontainers/data/fragmentargs/AddItemFragmentArg.kt
@@ -3,5 +3,9 @@ package com.hybris.plaincontainers.data.fragmentargs
 import java.io.Serializable
 
 class AddItemFragmentArg(
-    val containerId: Long
+    val containerId: Long,
+
+    // One-time-consume flag: Read for quick-add navigation and immediately set to false,
+    //  to prevent retrigger on navigateUp().
+    var navigateBarcodeFrag: Boolean = false
 ): Serializable {}
