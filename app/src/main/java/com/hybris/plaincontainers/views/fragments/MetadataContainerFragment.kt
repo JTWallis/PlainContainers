ab33b09e64f3fce92b65e1e8e9dc9f074f96bd26
Justin Wallis <wallis.justin123@gmail.com>
2026-06-30 12:21:41 +0200

    refactor: localized hardcoded colorpicker strings

--- app/src/main/java/com/hybris/plaincontainers/views/fragments/MetadataContainerFragment.kt
+++ app/src/main/java/com/hybris/plaincontainers/views/fragments/MetadataContainerFragment.kt
@@ -46,16 +46,20 @@ abstract class MetadataContainerFragment: MetadataBaseFragment() {
 
     private fun onColorClicked() {
         val builder = ColorPickerDialog.Builder(context)
-            .setTitle("Container Color")
-            .setPositiveButton("Apply", object: ColorEnvelopeListener {
-                override fun onColorSelected(envelope: ColorEnvelope, fromUser: Boolean) {
-                    setColor(envelope.color)
-                }
+            .setTitle(requireContext().getString(R.string.metadata_colorpicker_title))
+            .setPositiveButton(
+                requireContext().getString(R.string.metadata_btn_apply),
+                object: ColorEnvelopeListener {
+                    override fun onColorSelected(envelope: ColorEnvelope, fromUser: Boolean) {
+                        setColor(envelope.color)
+                    }
             })
-            .setNegativeButton("Cancel", object: DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface, i: Int) {
-                    dialog.dismiss()
-                }
+            .setNegativeButton(
+                requireContext().getString(R.string.metadata_popup_delete_cancel),
+                object: DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface, i: Int) {
+                        dialog.dismiss()
+                    }
             })
             .attachAlphaSlideBar(false)
 
