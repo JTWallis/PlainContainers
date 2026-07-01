72d32b7eaa150592ebfd5cb4d5a46e8d516969f2
Justin Wallis <wallis.justin123@gmail.com>
2026-07-01 11:24:20 +0200

    style: removed redundant semicolon

--- app/src/main/java/com/hybris/plaincontainers/BarcodeAnalyzer.kt
+++ app/src/main/java/com/hybris/plaincontainers/BarcodeAnalyzer.kt
@@ -12,39 +12,39 @@ class BarcodeAnalyzer(
     private val onBarcodeDetected: (String) -> Unit
 ) : ImageAnalysis.Analyzer {
 
-    private var isProcessing = false;
+    private var isProcessing = false
 
     @ExperimentalGetImage
     override fun analyze(imageProxy: ImageProxy) {
         if(isProcessing) {
-            imageProxy.close();
-            return;
+            imageProxy.close()
+            return
         }
 
-        val mediaImage = imageProxy.image;
+        val mediaImage = imageProxy.image
         if(mediaImage == null) {
-            imageProxy.close();
-            return;
+            imageProxy.close()
+            return
         }
 
-        isProcessing = true;
+        isProcessing = true
         val image = InputImage.fromMediaImage(
             mediaImage,
             imageProxy.imageInfo.rotationDegrees
-        );
+        )
 
         scanner.process(image)
             .addOnSuccessListener { barcodes ->
                 barcodes.firstOrNull()?.rawValue?.let {
-                    onBarcodeDetected(it);
+                    onBarcodeDetected(it)
                 }
             }
             .addOnFailureListener {
                 Log.e("BarcodeAnalyzer", "analyze: Failure on processing Barcode image")
             }
             .addOnCompleteListener {
-                isProcessing = false;
-                imageProxy.close();
+                isProcessing = false
+                imageProxy.close()
             }
 
     }
